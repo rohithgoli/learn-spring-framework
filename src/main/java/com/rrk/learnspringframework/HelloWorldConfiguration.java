243d497f0870bd3536ceaf78a6fd4f4ea03050e7
Rohith Goli <rohithgoli396@gmail.com>
2024-01-13 18:48:44 +0530

    feat: added more beans of custom classes - records - in the HelloWorldSpring

--- src/main/java/com/rrk/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/rrk/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,13 @@ package com.rrk.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Eliminates verbosity in creating Java Beans
+// Public accessor methods, constructor, equals, hashcode & toString are automatically created
+// Released in JDK 16
+record Person(String name, int age) {};
+
+record Address(String line1, String city, int pincode) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 	
@@ -11,4 +18,19 @@ public class HelloWorldConfiguration {
 		return "Ram";
 	}
 	
+	@Bean
+	public int age() {
+		return 16;
+	}
+	
+	@Bean
+	public Person person() {
+		var person = new Person("Laxman", 15);
+		return person;
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Ram mandir", "Ayodhya", 400342);
+	}
 }
