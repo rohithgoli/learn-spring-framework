cc6db02a97869035a1f7d1ea6445785d90b4a168
Rohith Goli <rohithgoli396@gmail.com>
2024-01-13 17:40:05 +0530

    feat: added HelloWorldSpring app with Spring context, empty Configuration class

--- src/main/java/com/rrk/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/rrk/learnspringframework/App01GamingBasicJava.java
@@ -5,15 +5,17 @@ import com.rrk.learnspringframework.game.MarioGame;
 import com.rrk.learnspringframework.game.PacManGame;
 import com.rrk.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 	
 //		MarioGame game = new MarioGame();
 //		SuperContraGame game = new SuperContraGame();
-		var game = new PacManGame();
+		var game = new PacManGame();	// 1: Object Creation
 		
 		var gameRunner = new GameRunner(game);
+			//2: Object Creation + Wiring of Dependencies
+			// Game is a Dependency of GameRunner
 		
 		gameRunner.run();
 	}
---
+++ src/main/java/com/rrk/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,18 @@
+package com.rrk.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		
+		//1. Launch a Spring Context
+		
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		
+		//2. Configure the things that we want Spring to manage - @Configuration
+		
+		
+	}
+	
+}
