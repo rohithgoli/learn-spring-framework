eeb7f50f0f1944549a2db554ee115077bba8a7c3
Rohith Goli <rohithgoli396@gmail.com>
2024-01-14 17:22:08 +0530

    feat: added tryWithResources for ApplicationContext in Spring helloworld

--- src/main/java/com/rrk/learnspringframework/helloworld/App02HelloWorldSpring.java
+++ src/main/java/com/rrk/learnspringframework/helloworld/App02HelloWorldSpring.java
@@ -9,29 +9,33 @@ public class App02HelloWorldSpring {
 	public static void main(String[] args) {
 		
 		//1. Launch a Spring Context
+		try(var context = 
+				new AnnotationConfigApplicationContext
+						(HelloWorldConfiguration.class)) {
+			
+			//2. Configure the things that we want Spring to manage - @Configuration
+			// HelloWorldConfiguration - @Configuration
+			// name - @Bean
+			
+			//3. Retrieving Beans managed by Spring
+			System.out.println(context.getBean("name"));
+			
+			System.out.println(context.getBean("age"));
+			
+			System.out.println(context.getBean("person"));
 		
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+			System.out.println(context.getBean("address2"));
 		
-		//2. Configure the things that we want Spring to manage - @Configuration
-		// HelloWorldConfiguration - @Configuration
-		// name - @Bean
+			System.out.println(context.getBean("person2MethodCall"));
+			
+			System.out.println(context.getBean("person3Parameters"));
 		
-		//3. Retrieving Beans managed by Spring
-		System.out.println(context.getBean("name"));
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			
+		}
 		
-		System.out.println(context.getBean("age"));
-		
-		System.out.println(context.getBean("person"));
-	
-		System.out.println(context.getBean("address2"));
-	
-		System.out.println(context.getBean("person2MethodCall"));
-		
-		System.out.println(context.getBean("person3Parameters"));
-	
-		
-		Arrays.stream(context.getBeanDefinitionNames())
-			.forEach(System.out::println);
 	}
 	
 }
