5974f3dab0ec45587c0acff516638f4e8a0b740f
Rohith Goli <rohithgoli396@gmail.com>
2024-01-16 10:53:36 +0530

    feat: added abt spring framework Bean scopes - Prototype, Singleton

---
+++ src/main/java/com/rrk/learnspringframework/beanScopeLauncherApplication/BeanScopeLauncherApplication.java
@@ -0,0 +1,53 @@
+package com.rrk.learnspringframework.beanScopeLauncherApplication;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+class NormalClass {
+	
+}
+
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+	
+}
+
+@Configuration
+@ComponentScan
+public class BeanScopeLauncherApplication {
+
+	public static void main(String[] args) {
+		
+		try(var context =
+				new AnnotationConfigApplicationContext
+				(BeanScopeLauncherApplication.class)) {
+			
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			// By default Singleton scope is applicable for Beans
+			// provides one instance per Spring IOC Container
+			
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			// provides multiple instance per Spring IOC Container
+		}
+		
+	}
+	
+}
